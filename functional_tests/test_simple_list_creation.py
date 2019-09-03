807d19c7fafa1b2272c6c2cb2da1dd4822884559
Ruslan <218.run@gmail.com>
2019-09-03 20:59:28 +0300

    Renamed all id and name in input elements of elements. Still not working

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -24,7 +24,7 @@ class NewVisitorTest(FunctionalTest):
         self.assertIn("To-Do", header_text)
 
         # Ей сразу же предлагается ввести элемент списка
-        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox = self.get_item_input_box()
         self.assertEqual(
             inputbox.get_attribute("placeholder"),
             "Enter a to-do item"
@@ -42,7 +42,7 @@ class NewVisitorTest(FunctionalTest):
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
         # (Эдит очень методична)
-        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox = self.get_item_input_box()
         inputbox.send_keys("Сделать мушку из павлиньих перьев")
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
@@ -58,7 +58,7 @@ class NewVisitorTest(FunctionalTest):
 
         # Эдит начинает новый список
         self.browser.get(self.live_server_url)
-        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox = self.get_item_input_box()
         inputbox.send_keys("Купить павлиньи перья")
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: Купить павлиньи перья")
@@ -81,7 +81,7 @@ class NewVisitorTest(FunctionalTest):
 
         # Фрэнсис начинает новый список, вводя новый элемент.
         # Он менее интересен, чем список Эдит...
-        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox = self.get_item_input_box()
         inputbox.send_keys("Купить молоко")
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: Купить молоко")

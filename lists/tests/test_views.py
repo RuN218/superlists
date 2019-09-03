a7d083022e1fb2fc56ae4d056c6044ed78f99b96
Ruslan <218.run@gmail.com>
2019-09-03 20:39:12 +0300

    The new form in home_page is applied, tests are simplified. NB broken logic

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.utils.html import escape
 
 from lists.models import Item, List
+from lists.forms import ItemForm
 
 
 class HomePageTest(TestCase):
@@ -11,6 +12,11 @@ class HomePageTest(TestCase):
         """тест: домашняя страница возвращает правильный html"""
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
+
+    def test_home_page_uses_item_form(self):
+        """тест: домашняя страница использует форму для элемента"""
+        response = self.client.get("/")
+        self.assertIsInstance(response.context["form"], ItemForm)
 
 
 class NewListTest(TestCase):
@@ -36,7 +42,6 @@ class NewListTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "home.html")
         expected_error = escape("You can't have an empty list item")
-        print(response.content.decode())
         self.assertContains(response, expected_error)
 
     def test_invalid_list_items_arent_saved(self):
